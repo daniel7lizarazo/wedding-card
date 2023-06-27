11df44fe0fb30e7685c6490a61fe403b18ce8ea4
DanielLizarazo <daniel7lizarazo@gmail.com>
2023-06-27 18:00:22 -0500

    invitados random fetch

---
+++ src/schemas/Invitados.ts
@@ -0,0 +1,13 @@
+export interface IInvitados {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: object;
+  location: object;
+  image: string;
+  url: string;
+  created: string;
+}
