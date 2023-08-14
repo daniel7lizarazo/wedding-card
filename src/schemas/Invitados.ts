77fde934b02eee48cca39f3eb1af1fd229bcaa80
DanielLizarazo <daniel7lizarazo@gmail.com>
2023-08-14 00:26:45 -0500

    pruebas htmx fructiferas

--- src/schemas/Invitados.ts
+++ src/schemas/Invitados.ts
@@ -1,7 +1,12 @@
+export interface IAsiste {
+  Bool: boolean;
+  Valid: boolean;
+}
+
 export interface IInvitados {
   Id: number;
   Id_text: string;
   Nombre: string;
   Nombre_invitacion: string;
-  Asiste: boolean;
+  Asiste: IAsiste;
 }
