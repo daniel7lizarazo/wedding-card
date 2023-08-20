a440d036f0b09627afcd45d25cc4af5adf4c66cb
DanielLizarazo <daniel7lizarazo@gmail.com>
2023-08-20 01:40:43 -0500

    Loader

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-  readonly PUBLIC_URL: string = "http://localhost:8080";
-  // readonly PUBLIC_URL: string = "https://wedding-back.fly.dev";
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
